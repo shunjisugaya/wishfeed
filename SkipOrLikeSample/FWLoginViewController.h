38ad39fd254ce787527f509c68b9f7a4c1478730
fxwx23 <fxwx23@gmail.com>
2014-06-17 20:18:31 +0900

    Add 3 viewcontroller

---
+++ SkipOrLikeSample/FWLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWLoginViewController.h
+//  Wishfeed_α
+//
+//  Created by Fumitaka Watanabe on 2014/06/17.
+//  Copyright (c) 2014年 Fumitaka Watanabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWLoginViewController : UIViewController
+
+@end
---
+++ SkipOrLikeSample/FWRegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWRegisterViewController.h
+//  Wishfeed_α
+//
+//  Created by Fumitaka Watanabe on 2014/06/17.
+//  Copyright (c) 2014年 Fumitaka Watanabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWRegisterViewController : UIViewController
+
+@end
